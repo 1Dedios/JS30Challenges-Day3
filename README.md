bef59669d800df4a330391bf9f3239d112fd4afb
Dedios <105307909+1Dedios@users.noreply.github.com>
2023-05-23 17:08:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# JavaScript 30 day Coding Challenge
+
+<p>This is day 2 of the JavaScript 30 vanilla Coding Challenges provided by WesBos. </p>
+
+Original challenges can be found here - [Challenge](https://courses.wesbos.com/account/access/62acf4628ed3995269ccb57c/view/194130581)
+
+## Deployed Site Link
+
+Github Pages (Interactive Site) - [Clock](https://1dedios.github.io/JS30Challenges-Day2/)
+
+## Differences Between Original and My Project:
+
+- I shortened the hour hand so you would be able to tell the hands apart.
+- Working on removing transition effect so when the second hand completes a full rotation, it does so smoothly. The original would "glitch" once it reached the full rotation.
+
+## Screenshot
+
+![Clock](Clock.png)
---
+++ script.js
@@ -0,0 +1,30 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90; // +90 because it was originally offset by 90 deg in the stylesheet
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+}
+
+function removeTransform(e) {
+
+}
+
+
+
+setInterval(setDate, 1000);
+
+setDate();
